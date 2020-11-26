5fa40d9064cc7eb3df308cf42a4d9d06f94e3563
Stephen Selao <stephenselaodev@gmail.com>
2020-11-25 19:51:02 -0500

    Fix non-null

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,13 @@ class App {
   static init() {
     const grid = new Grid();
 
-    const resetBtn = document.getElementById('reset')!;
+    const resetBtn = document.getElementById('reset') as HTMLButtonElement;
     resetBtn.addEventListener('click', grid.reset.bind(grid, false));
 
-    const endTurnBtn = document.getElementById('end-turn')!;
+    const endTurnBtn = document.getElementById('end-turn') as HTMLButtonElement;
     endTurnBtn.addEventListener('click', grid.changeTurns.bind(grid));
 
-    const spymasterBtn = document.getElementById('toggle-spymaster')!;
+    const spymasterBtn = document.getElementById('toggle-spymaster') as HTMLButtonElement;
     spymasterBtn.addEventListener('click', grid.toggleSpymasterView.bind(grid));
   }
 }
